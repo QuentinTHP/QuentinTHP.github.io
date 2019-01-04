47edc0da2d8c27d8c71a87c33ce1ceea7517fbeb
QuentinC <quentin.clere.pro@gmail.com>
2019-01-04 15:29:23 +0100

    Exo terminé

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 let monTitre = document.querySelector('h1');
 monTitre.textContent = 'Hello world!';
+
+
+window.setTimeout('if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"))window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");',5000)
+
+
